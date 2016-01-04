0d39978d75d619fb8fbfb9219e65b6c6f6a6b14a
sunnyxiaobai5 <sunnyxiaobai5@gmail.com>
2016-01-04 16:35:59 +0800

    grunt js，css合并压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,52 @@
 module.exports = function (grunt) {
     // Do grunt-related things in here
     grunt.initConfig({
-            pkg: grunt.file.readJSON('package.json'),
-            uglify: {
-                build: {
-                    src: 'src/<%=pkg.name%>.js',
-                    dest: 'build/<%pkg.name%>.min.js'
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                //separator: ';',
+                //stripBanners: true
+            },
+            dist: {
+                src: [
+                    'app/**/*.js',
+                    'app/js/app.js'
+                ],
+                dest: 'build/build.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'build/build.js',
+                dest: 'build/build.min.js'
+            }
+        },
+        cssmin: {
+            options: {
+                keepSpecialComments: 0
+            },
+            compress: {
+                files: {
+                    'assets/css/default.css': [
+                        "app/css/app.css",
+                        "css/pops.css",
+                        "css/index.css"
+                    ]
                 }
             }
         }
-    )
+    });
+
+    //载入concat，用于合并
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    //载入uglify，用于压缩
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //载入cssmin，用于css压缩及合并
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    //注册默认task
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 };
